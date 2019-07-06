16891790656972b0ab8527154f02477783d6f511
Simon Opheim <simon.opheim@gmail.com>
2019-07-06 18:14:46 +0200

    finished exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
@@ -70,6 +65,15 @@ const App = () => {
     </form>
     )
   }
+
+  const fetchHook = () => {
+    fetch('http://localhost:3001/persons')
+      .then(res => res.json())
+      .then(data => setPersons(data))
+      .catch(err => console.err(err))
+  }
+
+  useEffect(fetchHook, []);
 
   return (
     <div>

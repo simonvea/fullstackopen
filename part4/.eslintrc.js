f1fd0e209de30395c9f21dd685f1dd7530e0767d
Simon Opheim <simon.opheim@gmail.com>
2019-07-10 21:05:26 +0200

    finished 4.3-7

--- part4/.eslintrc.js
+++ part4/.eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
     "env": {
         "browser": true,
         "commonjs": true,
-        "es6": true
+        "es6": true,
+        "node": true,
+        "jest": true,
     },
     "extends": "eslint:recommended",
     "globals": {

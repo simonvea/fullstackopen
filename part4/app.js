ef6fe67cb757ef3d610185721fb09e7d06082621
Simon Opheim <simon.opheim@gmail.com>
2019-07-18 11:44:41 +0200

    finished part 7(ish)

--- part4/app.js
+++ part4/app.js
@@ -13,6 +13,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+mongoose.set('useFindAndModify', false);
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 
 app.use('/api/login', loginRouter)
